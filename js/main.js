9d80264595631106fcee93e1d83c80766d1f85a1
Aaron J. Zirbes <aaron.zirbes@gmail.com>
2013-10-16 13:47:59 -0500

    Adding debug

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(function() {
     var docMode = document.documentMode;
+    if (!docMode) {
+        docMode = "You are not running IE in compatability view."
+    }
     $("#documentMode").html(docMode);
+    console.log("docMode:" + docMode);
 });
 
